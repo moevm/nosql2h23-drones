2fd2daa0397ea69f9f633c46c4a424490b4c170a
fovteltar <nikitindima002@gmail.com>
2023-12-17 14:33:35 +0300

    cors_and_first_ajax

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import { PORT, HOST, URL_MAP } from './server-settings.js';
@@ -9,6 +10,7 @@ const HTML_DIR = join(SRC_DIR, 'html');
 
 const app = express();
 
+app.use(cors());
 app.use('/server-settings.js', express.static(join(__dirname, 'server-settings.js')));
 app.use(express.static(join(SRC_DIR, 'css')));
 app.use(express.static(join(SRC_DIR, 'js')));
@@ -16,12 +18,12 @@ app.use(express.static(join(SRC_DIR, 'js')));
 for (const addr of URL_MAP.keys()) {
   app.get(`/${addr}`, (req, res) => {
     res.sendFile(`${addr}.html`, {root: HTML_DIR});
-  })
+  });
 }
 
 app.get('*', (req, res) => {
   res.sendStatus(404);
-})
+});
 
 app.listen(PORT, HOST, () => {
   console.log(`App listening on port ${PORT}!`);
